edb066b8c73078068689f4c37e4a1bb0adfc8b6c
Hasnain Ahmed <156552529+HasnainSolangi@users.noreply.github.com>
2024-04-11 19:40:23 +0500

    Add files via upload

---
+++ cc.ts
@@ -0,0 +1,56 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+const currency: any = {
+    USD: 1,       // Base currency
+    PKR: 278, 
+    RUB: 92.58,
+    Yuan: 7.23,
+    INR: 83.30,
+};
+console.log(chalk.bgBlue("\n  Welcome to the Currency Converter Tool!"));
+while (true){
+let userAnswer = await inquirer.prompt(
+    [
+        {
+            name: "from",
+            message: "Enter From Currency",
+            type: "list",
+            choices: ["USD", "PKR", "RUB", "Yuan", "INR"]
+        },
+        {
+            name: "to",
+            message: "Enter To Currency",
+            type: "list",
+            choices: ["USD", "PKR", "RUB", "Yuan", "INR"]
+        },
+        {
+            name: "amount",
+            message: "Enter Your Amount",
+            type: "number"
+        }
+    ]
+)
+
+let fromAmount = currency[userAnswer.from] // Exchange rate
+let toAmount = currency[userAnswer.to] //Exchange rate
+let amount = userAnswer.amount
+let baseAmount = amount / fromAmount // PKR base currency
+let convertedAmount = baseAmount * toAmount
+console.log(convertedAmount);
+
+let confirm = await inquirer.prompt([
+    {
+        name: 'action',
+        type: "list",
+        message: chalk.green("Would you like to continue or exit?"),
+        choices: ['Continue', 'Exit']
+    }
+]);
+
+if (confirm.action === 'Exit') {
+    console.log(chalk.red("Thank you for using Currency Converter Tool!"));
+    break;
+ }
+}
